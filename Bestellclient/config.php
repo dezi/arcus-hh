bb49b7d0b95d592f52b68f52f9dba2c01d77f871
Dennis Zierahn <dezi@kappa-mm.de>
2016-09-14 09:57:15 +0200

    Check in.

---
+++ Bestellclient/config.php
@@ -0,0 +1,100 @@
+<?php
+
+$GLOBALS[ "benutzer" ] = array
+(
+	"ZDF_Mainz" => array
+	(
+		"pw" => "keins",
+		
+		"sources" => array
+		(
+			"AKT" => "Die Aktuelle",
+			"BR1" => "Brand1",		
+			"BRA" => "Bravo",		
+			"CIC" => "Cicero",	
+			"DNE" => "Das Neue",
+			"EMM" => "Emma",
+			"EPF" => "epd Film",
+			"FLT" => "Fluter",
+			"FZR" => "Freizeit Revue",
+			"HIG" => "Hintergrund",
+			"INY" => "International New York Times",
+			"MAM" => "manager magazin",
+			"MON" => "Le Monde",
+			"NPO" => "Neue Post",
+			"NUK" => "Natur + Kosmos",
+			"STA" => "7 Tage",
+			"STZ" => "Stuttgarter Zeitung",
+			"TAZ" => "Tageszeitung",
+			"TIM" => "Time",
+			"ZEN" => "Zenith"
+		)
+	),
+	
+	"ZDF_Berlin" => array
+	(
+		"pw" => "keins",
+		
+		"sources" => array
+		(
+			"TA1" => "TAZ Berlin Ausgabe",
+			"ND2" => "Neues Deutschland"
+		)
+	),
+	
+	"ZDF_Hamburg" => array
+	(
+		"pw" => "keins",
+		
+		"sources" => array
+		(
+			"TA5" => "TAZ Hamburg Ausgabe"
+		)
+	),
+);
+
+$GLOBALS[ "sachgebiete" ] = array
+(
+	"A",
+	"ASO",
+	"B",
+	"BAU",
+	"BBB",
+	"BEV",
+	"BIL",
+	"BIO",
+	"D",
+	"E",
+	"ENE",
+	"EXP",
+	"FNZ",
+	"GES",
+	"H",
+	"HHH",
+	"HIS",
+	"I",
+	"K",
+	"KAT",
+	"KUM",
+	"LEU",
+	"MIK",
+	"N",
+	"P",
+	"PLT",
+	"R",
+	"RAU",
+	"REL",
+	"RES",
+	"S",
+	"SPO",
+	"VER",
+	"W",
+	"WIR",
+	"WIS",
+	"X",
+	"XXX",
+	"Y",
+	"YP"
+);
+
+?>
---
+++ Bestellclient/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="de" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Hello PHP</title>
+<style type="text/css">
+.auto-style1 {
+	text-align: center;
+	font-size: xx-large;
+}
+</style>
+</head>
+
+<body>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p class="auto-style1"><strong>Hello PHP</strong></p>
+<?php echo "Test\n"; ahahssggs();?> 
+</body>
+
+</html>
